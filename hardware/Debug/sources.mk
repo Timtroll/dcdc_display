fd424e4f62547381fb7b35a1482b5fa8da8ebb7c
sergx961 <xuramshin.sergey@gmail.com>
2020-05-14 22:44:21 +0300

    add_hardware

---
+++ hardware/Debug/sources.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/BSP/Components/gde021a1 \
+Drivers/BSP/STM32L0538-Discovery \
+Drivers/STM32L0xx_HAL_Driver/Src \
+src/paper_screen \
+
